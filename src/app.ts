80f443bbbd658bcd3eaa48f91968972183d71c91
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-07-06 20:06:41 +0500

    Инициализация проекта
    -- Установка зависимостей --
    -- ts
    -- cors
    -- dotenv
    -- express
    -- body-parser
    
    -- app.ts --
    -- Создан класс App
    -- Запуск сервера

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const port = process.env.APP_PORT || 3000;
+
+class App {
+    private app: Express;
+
+    constructor() {
+        this.app = express();
+        this.app.use(express.urlencoded({ extended: true }));
+        this.app.use(bodyParser.json());
+        this.app.use(cors());
+    }
+
+    public init = async () => {
+        try {
+            this.app.listen(port, () => {
+                console.log(`\nServer is started on ${port}\n----------------------------------\n`);
+            });
+
+            this.app.get('/', (req: Request, res: Response) => {
+                res.send(`Server is running on port ${port}`);
+            });
+        } catch (error) {
+            const err = error as Error;
+            console.error(`\n${err.message}\n----------------------------------\n`);
+        }
+    };
+}
+
+export const app = new App();
+
+app.init();
