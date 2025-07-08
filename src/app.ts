cbf8e4d970c2cbab5da1d4a9301ba1b7bc026c71
SunnatBakidjanov <sunnatbackidjanov@gmail.com>
2025-07-08 11:13:12 +0500

    Создано подключение к базеданных
    --Создан файл db_connect.ts
    
    ----- db_connect.ts -----
    -- Создан класс DbConnect
    -- Создано подключение
    -- Создана функция connect с логикой подключения
    
    ----- app.ts ----
    -- Функция подключения вызвана в app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Express, Request, Response } from 'express';
+import DbConnect from './database/db_connect';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
 dotenv.config();
+
+const dbConnect = new DbConnect();
 
 const port = process.env.APP_PORT || 3000;
 
@@ -26,6 +29,8 @@ class App {
             this.app.get('/', (req: Request, res: Response) => {
                 res.send(`Server is running on port ${port}`);
             });
+
+            dbConnect.connect();
         } catch (error) {
             const err = error as Error;
             console.error(`\n${err.message}\n----------------------------------\n`);
@@ -33,6 +38,6 @@ class App {
     };
 }
 
-export const app = new App();
+const app = new App();
 
 app.init();
